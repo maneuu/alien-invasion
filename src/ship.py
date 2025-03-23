3532941daa31caa47af10649c3e74a95b7cd809c
eoemanoel@gmail.com <eoemanoel@gmail.com>
2025-03-23 11:34:46 -0300

    Refactor key event handling for improved readability and maintainability

--- src/ship.py
+++ src/ship.py
@@ -26,9 +26,9 @@ class Ship:
     def update(self):
         """Atualiza a posição da espaçonave com base na flag de movimento."""
         # Atualiza o valor da coordenada x da espaçonave, não o rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Atualiza o objeto rect a partir de self.x
